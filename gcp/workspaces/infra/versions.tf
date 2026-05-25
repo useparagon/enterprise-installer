9f0262c7ac16239153e85360c672009979dc5f46
Franklin Garcia <franklin.garcia@useparagon.com>
2026-05-25 13:04:17 -0400

    fix: removing version.tf

--- gcp/workspaces/infra/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      # broker_capacity_config on google_managed_kafka_cluster requires >= 7.19.0
-      version = ">= 7.19.0, < 8.0.0"
-    }
-  }
-}
