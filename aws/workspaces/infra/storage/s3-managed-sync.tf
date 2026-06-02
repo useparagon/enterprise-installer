9ab6a96e7e8c3ce7f04d49b27d02e941378d3597
Ted O'Connor <ted.oconnor@useparagon.com>
2026-06-02 17:25:27 -0400

    fix: incomplete s3 expiration rules

--- aws/workspaces/infra/storage/s3-managed-sync.tf
+++ aws/workspaces/infra/storage/s3-managed-sync.tf
@@ -56,7 +56,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "managed_sync" {
   bucket = aws_s3_bucket.managed_sync[0].id
 
   rule {
-    id = "expiration"
+    id     = "expiration"
+    status = "Enabled"
 
     filter {}
 
@@ -65,10 +66,23 @@ resource "aws_s3_bucket_lifecycle_configuration" "managed_sync" {
     }
 
     noncurrent_version_expiration {
-      noncurrent_days = var.app_bucket_expiration
+      noncurrent_days = 7
     }
 
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 1
+    }
+  }
+
+  rule {
+    id     = "markers"
     status = "Enabled"
+
+    filter {}
+
+    expiration {
+      expired_object_delete_marker = true
+    }
   }
 }
 
